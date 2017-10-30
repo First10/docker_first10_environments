4d704b1bdf843514be2f04c936de8816797b37d5
James Drawneek <jmdrawneek@googlemail.com>
2017-10-30 20:54:42 +0000

    Dockerfile updates

--- pushToHub.sh
+++ pushToHub.sh
@@ -6,9 +6,12 @@ DOCKER_ACCOUNT=jmdrawneek
 for f in ${FILES}*
 do
 
+# Only process files with Dockerfile in their name.
 if [[ ${f} == *Dockerfile* ]]; then
 
+# Convert string to lowercase.
 LOWERCASE=$(echo $(basename ${f}) | tr '[:upper:]' '[:lower:]')
+
   echo "Building ${LOWERCASE#*--} ..."
 
   docker build -f ${f} -t ${DOCKER_ACCOUNT}/${LOWERCASE#*--}:latest .
